40a0d6b2dc9acd51b90eabd861296b7641f410a4
Francesco Hartemann <fha@m2mfactory.fr>
2022-05-06 15:30:36 +0200

    change variables

--- lib/common/variables.dart
+++ lib/common/variables.dart
@@ -1,4 +1,4 @@
-const JOYA_URL = "http://192.168.54.147:5000/api/";
-const JOYA_SOCKET_URL = "http://192.168.54.147:5000";
+const JOYA_URL = "https://lejeunedeveloppeur.fr:5000/api/";
+const JOYA_SOCKET_URL = "http://lejeunedeveloppeur.fr:5000";
 const WIKI_URL =
     "https://fr.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=";
