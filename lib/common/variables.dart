50577633521259c52e67946eadccafac74940bdb
Francesco Hartemann <fha@m2mfactory.fr>
2022-05-06 15:35:25 +0200

    change url

--- lib/common/variables.dart
+++ lib/common/variables.dart
@@ -1,4 +1,4 @@
-const JOYA_URL = "https://lejeunedeveloppeur.fr:5000/api/";
+const JOYA_URL = "https://lejeunedeveloppeur.fr/api/";
 const JOYA_SOCKET_URL = "http://lejeunedeveloppeur.fr:5000";
 const WIKI_URL =
     "https://fr.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=";
